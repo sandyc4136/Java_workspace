0e83654180f92ca604bbece7856c58ffb6433eb4
trivia-sandyc4136 <sandyc4136@gmail.com>
2023-10-11 12:14:49 +0545

    Day_8.4 commit

---
+++ Day_8/Day_8.4/Inheritancee.java
@@ -0,0 +1,25 @@
+public class Inheritancee {
+ public static void main(String args[]){
+    Second s=new Second();
+    s.print();
+    s.myFun();
+ }  
+}
+class first{
+    int a=5;
+    int b=10;
+    void myFun(){
+        System.out.println("myFun of first class");
+    }
+}
+class Second extends first{
+    int c=15;
+    void anotherFun(){
+        System.out.println("Another fun of Second class");
+    }
+    void print(){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
