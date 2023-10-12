decf4ac30c9463a337546482c81ae8f301ca0df5
trivia-sandyc4136 <sandyc4136@gmail.com>
2023-10-13 00:36:59 +0545

    Final commit Day 9

--- Day_9/Day_9.8/FinalDemo.java
+++ Day_9/Day_9.8/FinalDemo.java
@@ -1,9 +1,37 @@
-public class FinalDemo {
+// public class FinalDemo {
+//     public static void main(String args[]){
+//         // int a;
+//         final int a=10;
+//         a=20;   // error: cannot assign a value to final variable a
+//         System.out.println(a); //error: variable a might not have been initialized
+//     }
+// }
+
+public class FinalDemo{
     public static void main(String args[]){
-        // int a;
-        final int a=10;
-        a=20;   // error: cannot assign a value to final variable a
-        System.out.println(a); //error: variable a might not have been initialized
+        Second s=new Second();
+        s.printDetail();
+    }
+}
+ class First{  
+    final int val=5;
+      void print(){
+        System.out.println(val);
+    }
+}
+class Second extends First{
+    int num=10;
+    void printDetail(){
+        super.print();
+        System.out.println(num);
     }
 }
 
+
+// final class First{
+    // int val=10;
+
+//}
+ //class Second extends First{ // Error  cannot inherit from final First
+    // code
+ //}
